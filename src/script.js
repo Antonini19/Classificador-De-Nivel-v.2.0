ae7775688e2c5ed95758a30daeb8bfe63cbd61c5
Antonini <leonardogodinho2014@gmail.com>
2025-02-04 20:14:38 -0300

    terceiro commit

--- src/script.js
+++ src/script.js
@@ -1,28 +1,36 @@
-let nomeDoHeroi = prompt("Digite o nome do seu heroi:")
-let monstros = prompt("digite a quantidade de mosntros que matou:")
-let xpColetado = monstros * 150
+let Programa = true
+while (Programa) {
+    let nomeDoHeroi = prompt("Digite o nome do seu heroi: (ou 'sair' para encerrar)")
+    if (nomeDoHeroi === "sair") {
+        Programa = false
+        } else {
+        
+        let monstros = prompt("digite a quantidade de mosntros que matou:")
+        let xpColetado = monstros * 150
 
-if (xpColetado<1000) {
-    Nivel = "ferro"
-} else if ((xpColetado>1001) && (xpColetado<2000)) {
-    Nivel = "Bronze"
-} else if((xpColetado >2001) && (xpColetado<5000)){
-    Nivel = "Prata"
-}else if((xpColetado >5001) && (xpColetado<7000)){
-    Nivel = "Ouro"
-}else if((xpColetado >7001) && (xpColetado<8000)){
-    Nivel = "Platina"
-}else if((xpColetado >8001) && (xpColetado<9000)){
-    Nivel = "Ascendente"
-}else if((xpColetado >9001) && (xpColetado<10000)){
-    Nivel = "Imortal"
-}else if(xpColetado>10001){
-    Nivel = "Radiante"
-}
+        if (xpColetado<1000) {
+        Nivel = "ferro"
+        }else if ((xpColetado>1001) && (xpColetado<2000)) {
+            Nivel = "Bronze"
+        }else if((xpColetado >2001) && (xpColetado<5000)){
+            Nivel = "Prata"
+        }else if((xpColetado >5001) && (xpColetado<7000)){
+            Nivel = "Ouro"
+        }else if((xpColetado >7001) && (xpColetado<8000)){
+            Nivel = "Platina"
+        }else if((xpColetado >8001) && (xpColetado<9000)){
+            Nivel = "Ascendente"
+        }else if((xpColetado >9001) && (xpColetado<10000)){
+            Nivel = "Imortal"
+        }else if(xpColetado>10001){
+            Nivel = "Radiante"
+        }
 
-console.log(
+        console.log(
     ["\nO Herói de nome:"+nomeDoHeroi]+
     ["\nEstá no nivel de:"+Nivel]+
     ["\nQuantidade de Mosntros mortos:"+monstros]+
     ["\nXp adquerido:"+xpColetado]
-    )
+                    )
+                }
+}

2816ad95e148575f14137aef3348d313fd5ccc41
Antonini <leonardogodinho2014@gmail.com>
2025-02-04 23:30:28 -0300

    Colocado a validação de login(melhorias1)

--- exercicio2/script2.js
+++ exercicio2/script2.js
@@ -1,27 +1,33 @@
+//função de verificação de login.
 function validarUsuario() {
     let nomeUsuario = prompt("Digite o nome do usuário:");
     let senha = prompt("Digite sua senha:");
 
     return nomeUsuario === "Leonardo" && senha === "123Antonini";
-}
+     // se as condicionais forem true a função sera true. se as condicionais forem false a função sera false.
+} 
+
+
 let continuar = true
-while (continuar) {
+while (continuar) { // loop 
     
 
-let acessoPermitido = validarUsuario();
+let acessoPermitido = validarUsuario(); //Guardando o valor boolean dentro da acessoPermitido
 
-if (acessoPermitido) {
+if (acessoPermitido) {// se o valor da função for true ele executa esse escopo:
 let valorSaque = prompt("Digite o valor do saque:");
 
             if (valorSaque <= 4500) {
-                console.log("Sacando:"+valorSaque);
-                continuar = false
+                console.log("Sacando:"+valorSaque); // se a condicional for true o loop se encerra.
+                continuar = false //loop encerrado
             } else {
                  console.log("Valor de saque inválido ou saldo insuficiente.");
              }
-    } else {
-    console.log("Acesso negado");
-    let resposta = prompt("deseja Voltar ao inicio?(sim/não)");
-        continuar = resposta === "sim";
-    }
+     // se a função boolean for false esse escopo vai ser executado        
+            } else { 
+                console.log("Acesso negado");
+
+                let resposta = prompt("deseja Voltar ao inicio?(sim/não)"); //add uma caixa para escrever se deseja voltar
+                continuar = resposta === "sim"; // se a resposta for true/ continuar fica true e volta e inicia o loop novamente.
+            }
 }
